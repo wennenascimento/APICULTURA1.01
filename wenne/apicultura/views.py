dc838b505c3339d190ef2d9a6c56350b52b0a0f8
Rayanecarlos <124010183+Rayanecarlos@users.noreply.github.com>
2023-02-27 21:36:55 -0300

    meu primeiro commitinho

---
+++ wenne/apicultura/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def start(request):
+ #return HttpResponse("Meu primeiro response html django")
+    return render(request, 'test.html')
